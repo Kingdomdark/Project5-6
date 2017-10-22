4e81a42bb803e9348a64e8dc9b7181972777aea5
ACJuicyJ <jasontjauw1999@outlook.com>
2017-10-22 21:46:58 +0200

    Login Edit
    Dammit when do I get help @here Usesless

---
+++ Webshop - Jason/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Ticketshop.Models;
+using Model;
+
+namespace Ticketshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Events()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+    [Route ("api/[controller]")]
+    public class MoviesController : Controller {
+        private readonly TicketContext _context;
+
+        public MoviesController (TicketContext context) {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet("Events")]
+        public IEnumerable<Theater> Get () 
+        {
+            return _context.Theaters.ToList ();;
+        }
+        [HttpGet ("Events/{id}")]
+        public IActionResult Get (int id) 
+        {
+            TicketContext ticketContext = new TicketContext();
+            var theater = ticketContext.Theaters.Single(Theater => Theater.TheaterID == id);
+            return Ok(theater);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post ([FromBody] Theater value) 
+        {
+
+        }
+
+        // PUT api/values/5
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] Theater value) 
+        {
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete ("{id}")]
+        public void Delete (int id) 
+        { 
+
+        }
+    }
+}
---
+++ Webshop - Jason/Models/Model.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using Microsoft.AspNetCore.Session;
+
+namespace Model
+{
+    //[(typeof(UserMetadata))]
+    // public partial class User
+    // {
+    //     public string ConfirmPassword{ get; set;}
+    // }
+    public class TicketContext : DbContext 
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Theater> Theaters{ get; set; }
+        public DbSet<Ticket> Tickets{ get; set; }
+        public DbSet<Customer> userAccount { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+          optionsBuilder.UseNpgsql("User ID=postgres;Password=;Host=localhost;Port=5432;Database=Ticketshop;Pooling=true;");
+        }
+    }
+
+    public class Customer {
+        [Key]
+        public int UserID { get; set; }
+        [Required(ErrorMessage = "Username is required.")]
+        [MaxLength(24)]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Last Name is required.")]
+        [MaxLength(24)]
+        public string Userlastname{ get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [MaxLength(50)]
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$", ErrorMessage = "Please enter a valid email.")]
+        public string email_adress{ get; set;}
+        [Required(ErrorMessage = "Password is required.")]
+        [MinLength(6)]
+        [DataType(DataType.Password)]
+        public string _password{ get; set;}
+        [Compare("Password", ErrorMessage = "Please confirm your password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword{ get; set; }
+    }
+
+    public class Theater {
+        [Key]
+        public int TheaterID { get; set; }
+        public string Theatername { get; set; }
+        public string Theateradress { get; set; }
+        public int Available_seats { get; set; }
+    }
+    public class Ticket {
+        [Key]
+        public int TicketID { get; set;}
+        public float Price { get; set;}
+        public string Eventname { get; set;}
+        public string Genre { get; set;}
+         //[ForeignKey]
+        public DateTime DateTime { get; set;}
+        public int TheaterID { get; set;}
+    } 
+    // public class UserMetadata
+    // {
+    //     [Display(Name = "Username")]
+    //     [Required(AllowEmptyStrings = false, ErrorMessage = "Username is required")]
+    //     public string Username { get; set; }
+
+    //     [Display(Name = "Last name")]
+    //     [Required(AllowEmptyStrings = false, ErrorMessage = "Last name is required")]
+    //     public string Userlastname { get; set; }
+
+    //     [Display(Name = "Email address")]
+    //     [Required(AllowEmptyStrings = false, ErrorMessage = "Email address is required")]
+    //     [DataType(DataType.EmailAddress)]
+    //     public string email_adress { get; set; }
+        
+    //     [Required(AllowEmptyStrings = false, ErrorMessage = "Password is required")]
+    //     [DataType(DataType.Password)]
+    //     [MinLength(6, ErrorMessage = "The password needs a minimum of 6 characters")]
+    //     public string _Password { get; set;}
+
+    //     [Display(Name = "Confirm Password")]
+    //     [DataType(DataType.Password)]
+    //     [Compare("Password", ErrorMessage = "The password does not match")]
+    //     public string ConfirmPassword { get; set; }
+    // }
+}
