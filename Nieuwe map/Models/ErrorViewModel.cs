4d955b370ef01c8572044edad36e1bf57aa73c59
ACJuicyJ <jasontjauw1999@outlook.com>
2017-10-23 16:12:57 +0200

    Login sqllite
    KILL ME

---
+++ Nieuwe map/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Nieuwe_map.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Nieuwe map/Models/ManageViewModels/GenerateRecoveryCodesViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nieuwe_map.Models.ManageViewModels
+{
+    public class GenerateRecoveryCodesViewModel
+    {
+        public string[] RecoveryCodes { get; set; }
+    }
+}
