e5fc87786838a571e19d18105a2978832ba42e42
LiWeiYeh <liwei_yeh@hotmail.com>
2017-11-24 11:50:07 +0100

    Deleted all shit + added Stefan's shit
    G A R B A G E

---
+++ Ticketshop/Ticketshop/Data/Migrations/20171124102751_Second.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Ticketshop.Data.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    OrderID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CustomerIDId = table.Column<string>(nullable: true),
+                    TicketID1 = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.OrderID);
+                    table.ForeignKey(
+                        name: "FK_Orders_AspNetUsers_CustomerIDId",
+                        column: x => x.CustomerIDId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Orders_Tickets_TicketID1",
+                        column: x => x.TicketID1,
+                        principalTable: "Tickets",
+                        principalColumn: "TicketID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_CustomerIDId",
+                table: "Orders",
+                column: "CustomerIDId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Orders_TicketID1",
+                table: "Orders",
+                column: "TicketID1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
