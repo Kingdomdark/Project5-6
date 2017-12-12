dc59b49472c0c77e7c696ceafd60c7b67fc4d459
LiWeiYeh <liwei_yeh@hotmail.com>
2017-12-12 09:21:04 +0100

    Model veranderd, Events filter is done

---
+++ Li-Wei/Bamboozle model versie/Ticketshop/Ticketshop/Models/EventsModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ticketshop.Models
+{
+    public class Event
+    {
+        [Key]
+        public int EventID { get; set; }
+        public float Price { get; set; }
+        [StringLength(45)]
+        public string Eventname { get; set; }
+        public string Genre { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int AvailableTickets { get; set; }
+        [StringLength(35)]
+        public string Theatername { get; set; }
+        [StringLength(50)]
+        public string Theateradress { get; set; }
+    }
+}
